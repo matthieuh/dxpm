b173840e94e4590a8b02b9c3487235cfae7564b9
Matthieu Hostache <matthieu.hostache@gmail.com>
2018-01-09 01:09:51 +0100

    Scaffold cli system on top of yarn

--- cli.js
+++ cli.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 'use strict';
-const updateNotifier = require('update-notifier');
-const meow = require('meow');
+const main = require('./lib/main');
 
-const cli = meow(`
-	Usage
-	  $ sf
-
-	Decentralyzed node package manager on top of ethereum
-`);
-
-updateNotifier({pkg: cli.pkg}).notify();
+main();
