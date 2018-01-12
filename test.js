7bf8eed6125be12ffb9c62851019f1e27a42444e
Matthieu Hostache <matthieu.hostache@gmail.com>
2018-01-12 23:32:56 +0100

    Add setup command

--- test.js
+++ test.js
@@ -5,3 +5,8 @@ test(async t => {
 	const {stdout} = await execa('./cli.js', ['--version']);
 	t.true(stdout.length > 0);
 });
+
+test('setup cmd should return something', async t => {
+	const {stdout} = await execa('./cli.js', ['setup']);
+	t.true(stdout.length > 0);
+});
